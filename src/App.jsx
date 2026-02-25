a1419fe3947f1dd28e83ff3ce9beaa56cb3de64d
RaulSamper <897466@unizar.es>
2026-02-25 03:51:14 +0100

    Mapa de aragon.jsx
    Se ha añadido el mapa de Aragon en jsx (coordenadas)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route, Link, Outlet } from 'react-router-dom';
-
+import MapaAragon from './MapaAragon';
 // Layout base que mantiene el Header y Footer fijos en todas las páginas
 const MainLayout = () => (
   <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
     {/* Header Base: Gestión de Sesión y Navegación */}
     <header style={{ padding: '1rem', borderBottom: '1px solid var(--border-ui)', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-      <h1 style={{ color: 'var(--primary-neon)', margin: 0, fontSize: '1.5rem' }}>SOBERANÍA</h1>
+      <h1 style={{ color: 'var(--primary-neon)', margin: 0, fontSize: '1.5rem' }}>SOBERANIA</h1>
       <nav>
         <Link to="/" style={{ color: 'white', marginRight: '1rem', textDecoration: 'none' }}>Login</Link>
         <Link to="/lobby" style={{ color: 'white', textDecoration: 'none' }}>Lobby</Link>
@@ -19,7 +19,7 @@ const MainLayout = () => (
 
     {/* Footer Base con información del sistema */}
     <footer style={{ padding: '1rem', borderTop: '1px solid var(--border-ui)', textAlign: 'center', opacity: 0.6 }}>
-      <small>Soberanía © 2026 - Universidad de Zaragoza - Proyecto Software </small>
+      <small>Soberania 2026 - Universidad de Zaragoza - Proyecto Software </small>
     </footer>
   </div>
 );
@@ -34,7 +34,11 @@ export default function App() {
           {/* RF.07: Gestión de Partidas */}
           <Route path="lobby" element={<div>Listado de Partidas (Lobby)</div>} />
           {/* RF.12: Representación del Entorno (Mapa de Aragón) */}
-          <Route path="partida/:id" element={<div>Tablero de Juego (Mapa Interactivo)</div>} />
+          <Route path="partida/:id" element={
+            <div style={{ width: '100%', height: '80vh' }}>
+              <MapaAragon />
+            </div>
+          } />
           {/* RF.05: Perfil y Estadísticas */}
           <Route path="perfil" element={<div>Estadísticas de Usuario</div>} />
         </Route>
